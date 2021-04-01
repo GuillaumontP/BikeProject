4af292805769348b802860bfad3a8da52b51db9f
Pierre Guillaumont <pierre.guillaumont@gmail.com>
2021-04-01 23:51:50 +0200

    last commit

--- endless.py
+++ endless.py
@@ -37,9 +37,10 @@ class Visualization(object):
         self.symbols_locations = [(None, None)]
         self._slider = None
         self._slider2 = None
+        
         title_widget = widgets.HTML(
             '<h3>Visualization bike flux Montpellier, France</h3>'
-            '<h4> size of circles growing by part of 150 bikers</h4>'
+            '<h4> size of circles growing every 150 bikers</h4>'
             '<h5>Inspired by  <a href="https://stackoverflow.com/questions/54857781/update-marker-in-gmaps-jupyter-using-widgets">StackOverFlow</a></h5>'
         )
         place = [[43.6162094554924, 3.8744080066680895],[43.6096992492676, 3.89693999290466],[43.61465, 3.8336],[43.5907, 3.81324], [43.615741799999995, 3.9096322], [43.626611, 3.895644], [43.6266977, 3.8956288],[43.6138841, 3.8684671], [43.57926, 3.93327], [43.578829999999996, 3.93324]]
@@ -51,19 +52,17 @@ class Visualization(object):
         display(self._container)
 #############################################################################################
 # mybinder
-    def on_button_clicked(self):
+    def update_date(self,b):
         day = self.FloatSlider1.value 
         month = self.FloatSlider2.value
-        print("Button clicked.")
-        day = ('0' + str(day))[-2:] # add 0 artificially 
-        month = ('0' + str(month))[-2:]
-        place = [[43.6162094554924, 3.8744080066680895],[43.6096992492676, 3.89693999290466],[43.61465, 3.8336],[43.5907, 3.81324], [43.615741799999995, 3.9096322], [43.626611, 3.895644], [43.6266977, 3.8956288],[43.6138841, 3.8684671], [43.57926, 3.93327], [43.578829999999996, 3.93324]]
+        day2 = ('0' + str(day))[-2:] # add 0 artificially 
+        month2 = ('0' + str(month))[-2:]
         Compt, dateofday = self.data()
-        nb_bikes = []
-        nb_days = sum(np.array(dateofday) < '2020-' + month + '-' + day) # carefull to First 0 index
+        
+        nb_days = sum(np.array(dateofday) < '2020-' + month2 + '-' + day2) # carefull to First 0 index
         for i in range(10):
-            nb_bikes.append(Compt[i][nb_days])
-        self.Gsymbols.markers[0].scale = 15
+            self.Gsymbols.markers[i].scale = round(Compt[i][nb_days]/150) + 1
+        
         return self._container
 
     def _render_control(self):
@@ -71,7 +70,7 @@ class Visualization(object):
         """ Render the widgets """
 
         self.FloatSlider1 = widgets.FloatSlider(
-        value = 15,
+        value = 16,
         min = 1,
         max = 31,
         step = 1,
@@ -94,15 +93,11 @@ class Visualization(object):
         readout = True,
         readout_format = '.0f',
         )
-    
-        self.button = widgets.Button(
-            description="Visualize Date"
-        )
+        
+        self.button = widgets.Button( description = "Visualize Date")
+        self.button.on_click(self.update_date)
+        controls = widgets.VBox([self.FloatSlider1, self.FloatSlider2, self.button])
 
-        self.button.on_click(self.on_button_clicked)
-
-        controls = widgets.VBox(
-        [self.FloatSlider1, self.FloatSlider2, self.button])
         return controls
 
     def _render_map(self, LatitudeLongitude):
@@ -121,4 +116,4 @@ class Visualization(object):
 
 
 Visualization().render()
-# %% 
+# %%
